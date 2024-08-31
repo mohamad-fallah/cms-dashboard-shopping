7fceac78834079f5ce75b2a97aa0e4c76abbca71
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-08-31 20:59:04 +0330

    finish header site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
 
+import './App.css'
 
 
 function App() {
   return (
+    <>
     <Sidebar/>
+    <div className="main">
+      <Header/>
+      {/* {router} */}
+    </div>
+    </>
   );
 }
 
